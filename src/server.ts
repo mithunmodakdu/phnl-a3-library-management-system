d4b686eccffbb1035952a7e8f9ffaff514f21555
Mithun4450 <132843404+Mithun4450@users.noreply.github.com>
2025-07-07 21:34:16 +0600

    project setup, book interface, bookSchema, Book model have been completed

---
+++ src/server.ts
@@ -0,0 +1,22 @@
+import {Server} from 'http';
+import mongoose from 'mongoose';
+import "dotenv/config"
+import app from './app';
+
+let server: Server;
+const PORT = process.env.PORT || 5000;
+
+async function main(){
+  try {
+    await mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.mcynqnr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`)
+    console.log("Successfully Connected to Mongodb using Mongoose");
+
+    server = app.listen(PORT, ()=>{
+      console.log(`Library Management App is listening on the port ${PORT}`)
+    })
+    
+  } catch (error) {
+    console.log(error)
+  }
+}
+main();

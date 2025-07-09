1160a59d0aa112c20f57caed72a0c2ce4f0a1a87
Mithun4450 <132843404+Mithun4450@users.noreply.github.com>
2025-07-09 21:32:09 +0600

    Update Book api by put method, Delete Book api, try catch statement use

--- src/app/controllers/book.controller.ts
+++ src/app/controllers/book.controller.ts
@@ -22,36 +22,121 @@ bookRoutes.post(
 );
 
 bookRoutes.get("/", async (req: Request, res: Response) => {
-  let books = [];
-  let bookGenre = req.query.filter? req.query.filter.toString().toUpperCase() : "";
-  const sortByField = req.query.sortBy;
-  const sortType = req.query.sort;
-  const limitNo = req.query.limit;
+  try {
+    let books = [];
+    const bookGenre = req.query.filter
+      ? req.query.filter.toString().toUpperCase()
+      : "";
+    const sortByField = req.query.sortBy;
+    const sortType = req.query.sort;
+    const limitNo = req.query.limit ? parseInt(req.query.limit.toString()) : 0;
 
-  if(bookGenre){
-    books = await Book.find({genre: bookGenre})
-    .sort({[sortByField as string]: sortType === "desc"? "desc": "asc"})
-    .limit(Number(limitNo));
-  } else{
-    books = await Book.find();
+    if (bookGenre) {
+      books = await Book.find({ genre: bookGenre })
+        .sort({ [sortByField as string]: sortType === "desc" ? "desc" : "asc" })
+        .limit(limitNo);
+    } else {
+      books = await Book.find();
+    }
+
+    if (books.length === 0) {
+      res.status(404).json({
+        success: false,
+        message: "Books Not Found",
+      });
+    }
+
+    res.status(200).json({
+      success: true,
+      message: "Books retrieved successfully",
+      data: books,
+    });
+  } catch (error: any) {
+    res.status(500).json({
+      success: false,
+      message: "Something went wrong",
+      error: error.message,
+    });
   }
-  
-  res.status(200).json({
-    success: true,
-    message: "Books retrieved successfully",
-    data: books,
-  });
 });
 
 bookRoutes.get("/:bookId", async (req: Request, res: Response) => {
-  const bookId = req.params.bookId;
-  console.log(bookId)
-  const book = await Book.findById(bookId);
-  console.log({book})
+  try {
+    const bookId = req.params.bookId;
+    const book = await Book.findById(bookId);
 
-  res.status(200).json({
-    success: true,
-    message: "Books retrieved successfully",
-    data: book,
-  });
+    if (!book) {
+      res.status(404).json({
+        success: false,
+        message: "Book not found",
+      });
+    }
+
+    res.status(200).json({
+      success: true,
+      message: "Book retrieved successfully",
+      data: book,
+    });
+  } catch (error: any) {
+    res.status(500).json({
+      success: false,
+      message: "Something went wrong",
+      error: error.message,
+    });
+  }
+});
+
+bookRoutes.put(
+  "/:bookId",
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const bookId = req.params.bookId;
+      const updateData = req.body;
+      const updatedBook = await Book.findByIdAndUpdate(bookId, updateData, {
+        new: true,
+        runValidators: true,
+      });
+
+      if (!updatedBook) {
+        res.status(404).json({
+          success: false,
+          message: "Book not found",
+        });
+      }
+      res.status(200).json({
+        success: true,
+        message: "Book updated successfully",
+        data: updatedBook,
+      });
+    } catch (error: any) {
+      next(error);
+    }
+  }
+);
+
+bookRoutes.delete("/:bookId", async (req: Request, res: Response) => {
+  try {
+    const bookId = req.params.bookId;
+    const deletedBook = await Book.findByIdAndDelete(bookId);
+
+    if (!deletedBook) {
+      res.status(404).json({
+        success: false,
+        message: "Book NOT Found",
+      });
+    }
+
+    res.status(200).json({
+      success: true,
+      message: "Book deleted successfully",
+      data: await Book.findById(bookId),
+    });
+
+  } catch (error: any) {
+    res.status(500).json({
+      success: false,
+      message: "Something went wrong",
+      error: error.message
+    })
+  }
 });

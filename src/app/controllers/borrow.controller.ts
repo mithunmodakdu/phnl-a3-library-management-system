3e17281691e342fc33c2871ebdf2eed2b42be372
Mithun4450 <132843404+Mithun4450@users.noreply.github.com>
2025-07-13 11:07:33 +0600

    Borrowed Books Summary (Using Aggregation) GET /api/borrow endpoint has been completed

--- src/app/controllers/borrow.controller.ts
+++ src/app/controllers/borrow.controller.ts
@@ -4,43 +4,89 @@ import { Book } from "../models/book.model";
 
 export const borrowRoutes = express.Router();
 
-borrowRoutes.post(
-  "/",
-  async (req: Request, res: Response, next: NextFunction) => {
-    try {
-      const borrowBody = req.body;
-      const quantity = borrowBody.quantity;
-      const bookId = borrowBody.book;
-      const book = await Book.findById(bookId);
-      console.log(borrowBody, book);
+borrowRoutes.post("/", async (req: Request, res: Response) => {
+  try {
+    const borrowBody = req.body;
+    const quantity = borrowBody.quantity;
+    const bookId = borrowBody.book;
+    const book = await Book.findById(bookId);
+    console.log(borrowBody, book);
 
-      if (book) {
-        if (quantity > book.copies) {
-          res.status(400).json({
-            success: false,
-            message: `Only ${book.copies} copies are available.`,
-          });
-        } else {
-          const updated = await book.updateAvailability(quantity);
-          console.log(updated);
-        }
-      } else {
-        res.status(404).json({
+    if (book) {
+      if (quantity > book.copies) {
+        res.status(400).json({
           success: false,
-          message: "Book NOT Found",
+          message: `Only ${book.copies} copies are available.`,
         });
+      } else {
+        const updated = await book.updateAvailability(quantity);
+        console.log(updated);
       }
-
-      const borrow = await Borrow.create(borrowBody);
-
-      res.status(200).json({
-        success: true,
-        message: "Book borrowed successfully",
-        data: borrow,
+    } else {
+      res.status(404).json({
+        success: false,
+        message: "Book NOT Found",
       });
-
-    } catch (error) {
-      next(error);
     }
+
+    const borrow = await Borrow.create(borrowBody);
+
+    res.status(200).json({
+      success: true,
+      message: "Book borrowed successfully",
+      data: borrow,
+    });
+  } catch (error: any) {
+    res.status(500).json({
+      success: false,
+      message: "Failed to borrow this book",
+      error: error.message,
+    });
   }
-);
+});
+
+borrowRoutes.get("/", async (req: Request, res: Response) => {
+  try {
+    const summary = await Borrow.aggregate([
+      {
+        $group: {
+          _id: "$book",
+          totalQuantity: { $sum: "$quantity" },
+        },
+      },
+      {
+        $lookup: {
+          from: "books",
+          localField: "_id",
+          foreignField: "_id",
+          as: "bookInfo",
+        },
+      },
+      {
+        $unwind: "$bookInfo",
+      },
+      {
+        $project: {
+          _id: 0,
+          book: {
+            title: "$bookInfo.title",
+            isbn: "$bookInfo.isbn",
+          },
+          totalQuantity: 1,
+        },
+      },
+    ]);
+    console.log(summary);
+    res.status(200).json({
+      success: true,
+      message: "Borrowed books summary retrieved successfully",
+      data: summary
+    });
+  } catch (error: any) {
+    res.status(500).json({
+      success: false,
+      message: "Failed to retrieve summary",
+      error: error.message,
+    });
+  }
+});
